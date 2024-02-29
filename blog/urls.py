c1e42655042e07d60918b4f20c064c50c1053250
dsmith1581 <124409577+dsmith1581@users.noreply.github.com>
2024-02-29 03:08:53 +0000

    Extras

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,18 @@
-from django.urls import path
+from django.urls import path, include
+from django.contrib import admin
+
 from . import views
+from django.contrib.auth import views as loginviews
+
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
+    path('post/new/', views.post_new, name='post_new'),
+    path('drafts/', views.post_draft_list, name='post_draft_list'),
+    path('post/<pk>/publish/', views.post_publish, name='post_publish'),
+    path('post/<pk>/remove/', views.post_remove, name='post_remove'),
+    path('accounts/logout/', loginviews.LogoutView.as_view(next_page='/'), name='logout'),
+    path('accounts/login/', loginviews.LoginView.as_view(), name='login'),
 ]
